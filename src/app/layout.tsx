b31dd1c8a1727bb4576c3b7dd0b6c101995faefc
Zackariah Lacey <zackariahlacey@gmail.com>
2026-03-13 16:10:40 -0400

    Design: Remove browser scrollbar for a seamless app-like experience

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -70,7 +70,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased no-scrollbar`}
       >
         <ScrollProgress />
         {children}
